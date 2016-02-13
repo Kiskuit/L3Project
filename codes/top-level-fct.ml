62c82ca1d57d9a2ffe3cf237a59691e0162bb6ac
Mathis Caristan <mathis.caristan@gmail.com>
2016-02-13 16:06:10 +0100

    get_fct now correctly returns a fct

--- codes/top-level-fct.ml
+++ codes/top-level-fct.ml
@@ -10,7 +10,7 @@ let number_of_fct = 2;;
 let get_filename =
     print_string "Please give the path to the file you want to hash : ";
     filename := read_line ();
-    print_string "\n------------------------------------------\n";;
+    print_string "\n------------------------------------------\n";
     print_string "------------------------------------------\n";;
 
 (* Asks user what will bethe size of the hashtable *)
@@ -19,9 +19,12 @@ let get_size =
         print_string "Please give the size of the hashTable in power of 2 (16 or 32) : 2^";
         size_of_hashtable := read_int ();
     done;
-    print_string "\n------------------------------------------\n";;
+    print_string "\n------------------------------------------\n";
     print_string "------------------------------------------\n";;
 
+(* TODO
+ * DUMMY function serving as placeholder *)
+let dummy_fct = ();;
 (* Asks which function will be used *)
 let get_fct =
     while ( !fct_to_use < 0 || !fct_to_use >= number_of_fct ); do
@@ -30,9 +33,13 @@ let get_fct =
         print_string "Please chose the function you want to use : ";
         fct_to_use := read_int();
     done;
-    print_string "\n------------------------------------------\n";;
-    print_string "------------------------------------------\n";;
-    print_string "------------------------------------------\n";;
+    print_string "\n------------------------------------------\n";
+    print_string "------------------------------------------\n";
+    print_string "------------------------------------------\n";
+    match !fct_to_use with
+    |0 -> dummy_fct
+    |1 -> dummy_fct
+    |_ -> dummy_fct;;
 
 get_filename;;
 get_size;;
