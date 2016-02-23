638f5584e426f248fad4802e70e6b1fbb8558d2b
Mathis Caristan <mathis.caristan@gmail.com>
2016-02-23 22:52:45 +0100

    Update comments

--- codes/myHash.c
+++ codes/myHash.c
@@ -5,8 +5,14 @@
 int MyHash(char* mot, int sizeTable){
 }
 
+/* This function receives a word *mot* in plain text (char*)
+ * It codes every letter capital or not indistinctly onto a 
+ * number between 1 and 26.
+ * Digits are coded to numbers between 27 and 36 (TODO).
+ * Every other caracters are ignored (coded to 0).
+ * Transformation is such that in the end, every character
+ * of *mot* can be encoded onto 6bits */
 void Transform(char* mot){
-    //TODO TAKE INTO ACCOUNT 0-9
     while(mot++){
         if( 65<=mot && mot<=90 )
             mot-=65;
@@ -17,28 +23,31 @@ void Transform(char* mot){
     }
 }
 
-void Chunks(char* mot, int* retour, int* size){
+/* This function receives a word *mot* that must already have been
+ * transformed. *size* can point to any INT and WILL be modified.
+ * Chunks a word into parts of (SIZECHUNK*6) bits long */
+unsigned int *Chunks(char* mot, int* size){
     //TODO DEPEND ON PARAM SIZECHUNK INSTEAD OF 3
     char motBis = mot;//copy of the pointer
     int len = 0;
     while (motBis++)//Getting the length of the word
         len++;
     *size = (int) ceil(len/3);
-    retour = malloc(sizeof(int)*size);
+    retVal = malloc(sizeof(unsigned int)*size);
     
     int shift = 0,i = 0;
     while(mot++){//Parcours du mot
         if(shift > 12)
             shift = 0;
-        int tmp = *mot;
+        unsigned int tmp = *mot;
         tmp << shift;
         chunk |= tmp;
         i++;
         if(i==3){
             i = 0;
-            retour[0] = chunk;
+            retVal[0] = chunk;
             chunk = 0;
-            retour++;
+            retVal++;
         }
         shift += 6
     }
