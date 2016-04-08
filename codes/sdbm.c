d86e40f8ce3bfbabc30807f418b555cdfcac958f
Shyuto <florian.reynier@gmail.com>
2016-04-08 23:59:33 +0200

    Update sdbm.c
    added interface with Ocaml

--- codes/sdbm.c
+++ codes/sdbm.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 
+CAMLprim value  sdbm_main(value ml_array){
+	CAMLparam1( ml_array );
+        const char* s = String_val(Field(ml_array,0));
+        const int shift = 32 - atoi(String_val(Field(ml_array,1)));
+	int len = strlen(s);
+	 return Val_int(Sdbm(s,len)) >> shift;
+	
+}
+
 unsigned long Sdbm(char* mot, unsigned long sizeTable){
     unsigned long retVal=0;
     int c;
