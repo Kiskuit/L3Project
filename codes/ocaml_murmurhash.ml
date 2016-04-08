ffae6d376450297efc7bdcc4704dd33709fd71f5
Shyuto <florian.reynier@gmail.com>
2016-04-08 23:58:42 +0200

    Update ocaml_murmurhash.ml
    support of hash size

--- codes/ocaml_murmurhash.ml
+++ codes/ocaml_murmurhash.ml
@@ -1,3 +1,7 @@
-external real_murmurhash: string array -> int = "murmurhash_mask"
+external real_murmurhash: string array -> int = "murmurhash_main"
 
-let murmurhash mot size = real_murmurhash[|mot|];;
+let murmurhash mot size = 
+    match size with
+    |8 -> real_murmurhash [|mot;"8"|]
+    |16 -> real_murmurhash [|mot;"16"|]
+    |_ -> real_murmurhash [|mot;"30"|];;
